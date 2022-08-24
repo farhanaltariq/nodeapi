37067335edf2c9deaba4ec678f2169d32b00fcdf
Farhan Altariq <farhanaaxy@gmail.com>
2022-08-24 23:55:17 +0700

    update function to connect with atlas

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
 export default function mongooseConnectDB() {
     mongoose
-        .connect("mongodb://localhost:27017/rfid", {
+        .connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
