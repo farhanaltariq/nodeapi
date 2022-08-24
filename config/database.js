14b7ca2ceb6e2bdf9308b24bca2acdfae88ddb91
Farhan Altariq <farhanaaxy@gmail.com>
2022-08-25 00:17:54 +0700

    update connection to atlas

--- config/database.js
+++ config/database.js
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
-
-export default function mongooseConnectDB() {
-    mongoose
-        .connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Connected to database");
-        })
-        .catch((err) => console.log(`Fail to connect, ${err}`));
+export class MongooseConnection {
+    static connectDB = async (req, res) => {
+        mongoose
+            .connect(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .then(() => {
+                console.log("Connected to database");
+            })
+            .catch((err) => console.log(`Fail to connect, ${err}`));
+    };
 }
--- index.js
+++ index.js
@@ -2,20 +2,18 @@ import express from "express";
 import cors from "cors";
 import route from "./routes/index.js";
 import dotenv from "dotenv";
-import mongooseConnectDB from "./config/database.js";
+import { MongooseConnection } from "./config/database.js";
 
 const app = express();
 dotenv.config();
 
-mongooseConnectDB.apply();
+MongooseConnection.connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use("/rfid", route);
+app.use(route);
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-    console.log(`Server running at "http://${host}:${port}"`)
-);
+app.listen(port, () => console.log(`Server running at http://${host}:${port}`));
