1f6f15050fead21f20c0a90ad99ea1239d785d8f
Farhan Altariq <farhanaaxy@gmail.com>
2023-04-04 09:44:36 +0700

    another attempt to deploy in vercel

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ MongooseConnection.connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
